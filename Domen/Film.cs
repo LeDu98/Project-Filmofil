f88d4e34e4d9cdbca2371f902396727d1e6acae4
Dušan Gajić <dg20170149@student.fon.bg.ac.rs>
2022-01-03 14:44:38 +0100

    Add project files.

---
+++ Domen/Film.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domen
+{
+    public class Film
+    {
+        public int FilmId { get; set; }
+        public string Naziv { get; set; }
+        public string Zanr { get; set; }
+        public int Trajanje { get; set; }
+        public double Ocena { get; set; }
+        public string Thumbnail { get; set; }
+        public string Trailer { get; set; }
+        public int StudioId { get; set; }
+        public Studio Studio { get; set; }
+
+    }
+}
---
+++ Domen/Studio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domen
+{
+    public class Studio
+    {
+        public int StudioId { get; set; }
+        public string Naziv { get; set; }
+        public string Sediste { get; set; }
+        public DateTime DatumOsnivanja { get; set; }
+
+    }
+}
