020833544d5f09079e698ccb53cd86f08e2a6725
MilosVu <vujic.milos1998@gmail.com>
2022-01-08 14:58:57 +0100

    ASP projekt kreiran

--- Domen/User.cs
+++ Domen/User.cs
@@ -9,11 +9,19 @@ namespace Domen
    public class User
     {
         public int UserId { get; set; }
+
         public string FirstName { get; set; }
+
         public string LastName { get; set; }
+
         public string Email { get; set; }
+
         public string Username { get; set; }
+
         public string Password { get; set; }
+
+        public bool IsAdministrator { get; set; }
+
         public List<Movie> Movies { get; set; }
     }
 }
