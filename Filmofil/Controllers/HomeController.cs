19a2cff24e508568f95a287a5c98d095607f671c
Dušan Gajić <dg20170149@student.fon.bg.ac.rs>
2022-01-09 19:03:05 +0100

    HomePage

---
+++ Filmofil/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Domen;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Filmofil.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var user = JsonSerializer.Deserialize<User>(HttpContext.Session.Get("user"));
+            if (user is null) {
+                return RedirectToAction("Index", "Login");
+            }
+            return View();
+        }
+    }
+}
