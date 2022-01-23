3cf8757ee2a22226ead579805b6d41285fe6a5b4
Dušan Gajić <dg20170149@student.fon.bg.ac.rs>
2022-01-23 15:16:45 +0100

    Dashboard panel

---
+++ Filmofil/Controllers/DashboardController.cs
@@ -0,0 +1,67 @@
+﻿using DataAccesLayer.UnitOfWork;
+using Domen;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Filmofil.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly IUnitOfWork unitOfWork;
+        private readonly IHostingEnvironment hostingEnvironment;
+
+        public DashboardController(IUnitOfWork unitOfWork, IHostingEnvironment hostingEnvironment)
+        {
+            this.unitOfWork = unitOfWork;
+            this.hostingEnvironment = hostingEnvironment;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Actors()
+        {
+            List<Actor> model = unitOfWork.ActorRepository.GetAll().ToList();
+
+            return View(model);
+
+        }
+
+        public IActionResult Personnels()
+        {
+            List<Personnel> model = unitOfWork.PersonnelRepository.GetAll().ToList();
+
+            return View(model);
+
+        }
+
+        public IActionResult Movies()
+        {
+            List<Movie> model = unitOfWork.MovieRepository.GetAll().ToList();
+
+            return View(model);
+
+        }
+
+        public IActionResult StreamingServices()
+        {
+            List<StreamingService> model = unitOfWork.StreamingServiceRepository.GetAll().ToList();
+
+            return View(model);
+
+        }
+
+        public IActionResult Studios()
+        {
+            List<Studio> model = unitOfWork.StudioRepository.GetAll().ToList();
+
+            return View(model);
+
+        }
+    }
+}
